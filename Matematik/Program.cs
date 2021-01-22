dd75cc958e288661ecfcbacdc2499b7ddb220a09
silanharmancii <silanharmanci.9@gmail.com>
2021-01-22 14:01:28 +0300

    Add project files.

---
+++ Matematik/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Matematik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DortIslem dortislem = new DortIslem();
+            dortislem.Topla(2,3);
+            Console.ReadLine();
+        }
+        
+    }
+}
